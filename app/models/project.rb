e24cde1ecfccff6b62c27491eeb58034d342d182
Nguyen Ton Hung <hungnt@hoangkhang.com.vn>
2016-03-29 16:35:25 +0700

    update

--- app/models/project.rb
+++ app/models/project.rb
@@ -11,4 +11,10 @@ class Project < ActiveRecord::Base
       self.joins(:project_tag).where("project_tags.title_vn = 'Công trình đang thi công'").order("created_at DESC")
     end
   end
+  def total_value=(new_total_value)
+      self[:total_value] = new_total_value.to_s.gsub(/[\,]/, ',')
+  end
+  def cost_performance=(new_cost_performance)
+      self[:cost_performance] = new_cost_performance.to_s.gsub(/[\,]/, ',')
+  end
 end
